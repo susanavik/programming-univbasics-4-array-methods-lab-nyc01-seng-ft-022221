a8e6d32068047eaa888bc9bd617e94e6028a950f
Susana vik <susana.m.vik@gmail.com>
2021-02-09 17:00:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
-
+  array = ["element1", "array"]
+  array.include?("element")
 end
 
 def using_sort(array)
-
+  array = ["I", "am", "wow"]
+  array.sort
 end
 
 def using_reverse(array)
-
+  array = ["wow", "I", "love", "arrays!"]
+  array.reverse
 end
 
 def using_first(array)
-
+  array = ["wow", "this", "is", "cool"]
+  array.first
 end
 
 def using_last(array)
-
+  array = ["I", "love", "arrays!"]
+  array.last
 end
 
 def using_size(array)
-
+  array = ["dog", "pig", "horse", "sheep", "goat", "chicken"]
+  array.size
 end
